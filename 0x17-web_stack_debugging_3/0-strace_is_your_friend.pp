9d279d371d2cf2cd6a9733f8abf4c37a2eb02603
Masaya Elvin <masayaelvin@gmail.com>
2024-01-18 14:48:13 +0300

    changes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
 exec { 'fix-wordpress':
-  command => "sed -i /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php"
+	command => "sed -i /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php"
+	path    => '/usr/local/bin/:/bin/'
 }
